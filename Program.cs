e2df9328f4e6402bdcf0926f1c905a38fbbdc54d
Henrik Klinkmann <hkl@iki.fi>
2020-05-05 15:32:34 +0300

    Add code

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+
+namespace BluDenim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+        static void Pull()
+        {
+            const string UserFullname = "Henrik Klinkmann";
+            const string UserEmail = "hkl@iki.fi";
+
+            using var repo = new Repository(@"c:\git\BluDenim");
+
+            // Credential information to fetch
+            var options = new PullOptions
+            {
+                FetchOptions = new FetchOptions()
+            };
+
+            //options.FetchOptions.CredentialsProvider = new CredentialsHandler(
+            //    (url, usernameFromUrl, types) =>
+            //        new UsernamePasswordCredentials()
+            //        {
+            //            Username = USERNAME,
+            //            Password = PASSWORD
+            //        });
+
+            // User information to create a merge commit
+            var signature = new Signature(new Identity(UserFullname, UserEmail), DateTimeOffset.Now);
+
+            // Pull
+            Commands.Pull(repo, signature, options);
+        }
+    }
+}
